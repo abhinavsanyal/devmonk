434091fbc6ef5d84c78d0f2df0e8ba5a7c6837af
Abhinava <reltronx@gmail.com>
2018-05-17 17:47:07 +0530

    s3 bucket imge uploads

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(port,(err)=>{
